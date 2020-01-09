b4cb58d54aa217dbfdbe1b634018e2e63fa7b679
cparpaillon <camille.grison@gmail.com>
2020-01-09 21:09:57 +0100

    love icon

--- tshirtz-frontend/src/components/Footer.jsx
+++ tshirtz-frontend/src/components/Footer.jsx
@@ -3,7 +3,11 @@ import "../footer.css";
 
 // to put in the footer : https://codepen.io/kevinjannis/pen/Krids
 function Footer() {
-  return <div className="fixed-footer">Made with tea by Camille</div>;
+  return (
+    <div className="fixed-footer">
+      Made with <i class="icon ion-heart"></i> by Camille
+    </div>
+  );
 }
 
 export default Footer;
