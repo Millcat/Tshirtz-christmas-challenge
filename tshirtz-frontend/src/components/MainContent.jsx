5b5c0758c00c72739b7e336082d41dace11ad476
cparpaillon <camille.grison@gmail.com>
2020-01-09 19:53:13 +0100

    final combo filters in getFilteredTshirts

--- tshirtz-frontend/src/components/MainContent.jsx
+++ tshirtz-frontend/src/components/MainContent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Filters from "./Filters";
 import TshirtsList from "./TshirtsList";
 import tshirtsData from "../data/tshirts.json";
@@ -6,29 +6,35 @@ import tshirtsData from "../data/tshirts.json";
 function MainContent(props) {
   const [tshirts, setTshirts] = useState(tshirtsData.data);
 
-  useEffect(() => {
-    // get the filtered Tshirts by 3 filters : price, brands, colors. All empty array by default:
-    // getFilteredTshirts([], [], []);
-    // setTshirts(tshirtsData.data);
-  }, []);
+  const getFilteredTshirts = (maxPrice, brands, colors) => {
+    console.log("you are in get filteredTshirts: ");
 
-  const getFilteredTshirts = (prices, brands, colors) => {
-    console.log(
-      "you are in get filteredTshirts: ",
-      "prices ==>",
-      prices,
-      "brands ==>",
-      brands,
-      "colors ==>",
-      colors
-    );
+    const colorsName = colors
+      .filter(color => color.isSelected)
+      .map(color => color.name);
+    // ex: ["Blue", "Yellow"]
+
+    const brandsName = brands
+      .filter(brand => brand.isSelected)
+      .map(brand => brand.name);
+    // ex: ["Celio", "Dior"]
+
+    const filteredTshirts = tshirtsData.data.filter(tshirt => {
+      return (
+        (brandsName.length === 0 || brandsName.includes(tshirt.brand)) &&
+        (colorsName.length === 0 || colorsName.includes(tshirt.color)) &&
+        tshirt.price <= maxPrice
+      );
+    });
+
+    setTshirts(filteredTshirts);
   };
 
   return (
     <div className="main-content">
       <Filters
-        onFilters={(prices, brands, colors) =>
-          getFilteredTshirts(prices, brands, colors)
+        onFilters={(maxPrice, brands, colors) =>
+          getFilteredTshirts(maxPrice, brands, colors)
         }
       />
       <TshirtsList tshirts={tshirts} />
